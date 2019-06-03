748f20e6f8441a1cdea5578be4737aa8afd52fca
DeepLearning VM <google-dl-platform@googlegroups.com>
2019-06-03 03:16:58 +0000

    Changed const file path name

--- const.py
+++ const.py
@@ -6,7 +6,7 @@ TEST_PATH = "/home/parsley789/data/test/"
 TRAIN_CSV_PATH = "~/flooding/data/train_data.csv"
 VAL_CSV_PATH = "~/flooding/data/val_data.csv"
 TEST_CSV_PATH = "~/flooding/data/test_data.csv"
-VGG_FLOOD_PATH = "/home/parsley789/data/vgg16_flood.pt"
+VGG_TRAINED_PATH = "/home/parsley789/data/vgg16_flood.pt"
 
 PROTOFILE = "utils/residual-attention-network/imagenet_model/AttentionNeXt-56-deploy.prototxt"
 
